6f3b8ab81a536a1f434fb955bb935418bd24f9c8
guzmanlopez <guzilop@gmail.com>
2014-11-20 15:28:03 -0200

    modificar contenido.md y ui

--- Texto/contenido.md
+++ Texto/contenido.md
@@ -2,40 +2,39 @@
 output: html_document
 ---
 
-PAS-CIPAC <a name="pascipac"></a><img style="float: right" src="https://raw.githubusercontent.com/guzmanlopez/PAS-DINARA/master/Iconos%20y%20figuras/LOGO_PAS_CIPAC_web.jpg">
-==================
+<div style="clear: right;"><img src="https://raw.githubusercontent.com/guzmanlopez/PAS-DINARA/master/Iconos%20y%20figuras/LOGO_PAS_CIPAC_web.jpg" alt="" style="float: right; margin-right:20px"/></div> 
 <br>
-El PAS-CIPAC es un grupo de investigadores del Programa de Arqueología Subacuática (PAS) de la Facultad de Humanidades y Ciencias de la Eduacación y del Centro de Investigación del Patrimonio Costero (CIPAC) en el Centro Universitario de la Región Este (CURE), ambas instituciones de la Universidad de la República - Uruguay. Hoy día el PAS-CIPAC constiuye una Unidad Asociada de la universidad compartiendo recursos humanos y materiales, para la investigación cientifica, gestion, enseñanza y divulgacion en la tematicas del Patrimonio Cultural Subacuático, maritimo y costero en el Uruguay.
+
+## PAS-CIPAC <a name="pascipac"></a>
 <br>
-<br>
-DINARA <a name="dinara"></a>![Image](https://raw.githubusercontent.com/guzmanlopez/PAS-DINARA/master/Iconos%20y%20figuras/logo_DINARA.png)
-==================
-<br>
-La Dirección Nacional de Recursos Acuáticos (DINARA), es la entidad estatal responsable de regular y controlar la actividad pesquera y acuícola en Uruguay. La DINARA pertenece al Ministerio de Ganadería, Agricultura y Pesca (MGAP) de Uruguay y es quien define y ejecuta la política pesquera y acuícola del gobierno nacional.
+<p style="text-align:justify">El PAS-CIPAC es un grupo de investigadores del Programa de Arqueología Subacuática (PAS) de la Facultad de Humanidades y Ciencias de la Eduacación y del Centro de Investigación del Patrimonio Costero (CIPAC) en el Centro Universitario de la Región Este (CURE), ambas instituciones de la Universidad de la República - Uruguay. Hoy día el PAS-CIPAC constiuye una Unidad Asociada de la universidad compartiendo recursos humanos y materiales, para la investigación cientifica, gestion, enseñanza y divulgacion en la tematicas del >Patrimonio Cultural Subacuático, maritimo y costero en el Uruguay.</p>
 <br>
 <br>
+</a><div style="clear: right;"><img src="https://raw.githubusercontent.com/guzmanlopez/PAS-DINARA/master/Iconos%20y%20figuras/logo_DINARA.png" alt="" style="float: right; margin-right:20px"/></div>
 <br>
+
+## DINARA <a name="dinara"></a>
 <br>
-Bahía de Maldonado <a name="bdm"></a>
-==================
+<p style="text-align:justify">La Dirección Nacional de Recursos Acuáticos (DINARA), es la entidad estatal responsable de regular y controlar la actividad pesquera y acuícola en Uruguay. La DINARA pertenece al Ministerio de Ganadería, Agricultura y Pesca (MGAP) de Uruguay y es quien define y ejecuta la política pesquera y acuícola del gobierno nacional.</p>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-Primer salida: 06/06/2014 <a name="bdm1"></a>
+
 ------------------------------------------------------------------------
+
+### Bahía de Maldonado <a name="bdm"></a>
 <br>
-La primer salida fue un éxito. 
+
+#### Primer salida de agua: 06/06/2014 <a name="bdm1"></a>
+<br>
+
+##### Objetivos:
+* probar sonar de barrido lateral y cámara de video sumergible.
+
+En el marco de la propuesta “Caracterización del fondo marino en la Bahía de Maldonado (inmediaciones de la Isla Gorriti) y evaluación de mejillón (Mytilus edulis platensis)” se realizó una salida de campo para probar equipos científicos. La misma consistió en remolcar un sonar de barrido lateral modelo StarFish 990F de Tritech desde una embarcación semi-rígida modelo Zodiac y registrar las características del fondo marino. A través de los registros acústicos obtenidos se pudo distinguir en tiempo real zonas rocosas y arenosas del fondo marino (Ver Figura 1).
+
+![Image]()
+
+Figura 1 – Imagen de sonar de barrido lateral de un fondo marino arenoso con marcas de ripples (A) y fotograma de filmación subacuática del mismo tipo de fondo (B). En C se muestra un fondo marino rocoso.  
+
+
+------------------------------------------------------------------------
+
--- ui.R
+++ ui.R
@@ -17,7 +17,50 @@ shinyUI(navbarPage(title="Convenio PAS-DINARA",
              fluid=TRUE,
              responsive=TRUE,
              
-             tabPanel("Cargar archivo SBL", icon=icon(name="file", "fa-2x"),
+                         
+             tabPanel("Bitácora",
+                      icon=icon(name="location-arrow", "fa-2x"),
+                      fluidRow(
+                        column(2,
+                               offset=0.5,
+                               includeMarkdown("/home/guzman/GitHub/PAS-DINARA/Texto/indice.md")
+                        ),
+                        column(10,
+                               offset=-0.5,
+                               includeMarkdown("/home/guzman/GitHub/PAS-DINARA/Texto/contenido.md")
+                               )
+                        )
+                      ),
+             
+             tabPanel("SIG",
+                      icon=icon(name="map-marker", "fa-2x"),
+                      div(class="outer",
+                          tags$head(
+                            # Include our custom CSS
+                            includeCSS("styles.css"),
+                            includeScript("gomap.js"),
+                            includeScript("binding.js")
+                            ),
+                          leafletMap("map", width="100%", height="100%",
+                                     initialTileLayer = "//{s}.tiles.mapbox.com/v3/guzman.j035h3hc/{z}/{x}/{y}.png",
+                                     initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
+                                     options=list(center= c(-34.9400, -54.9700),
+                                                  zoom = 13,
+                                                  maxBounds = list(list(-32.99966,-49.49774), list(-40.00023,-59.50186))
+                                                  )
+                                     ),
+                          absolutePanel(id = "controls", class="modal", fixed=TRUE, draggable=TRUE, top=120, left=300, right="auto", bottom="auto", width=200, height="auto",
+                                        h4("Capas espaciales"),
+                                        tags$hr(),
+                                        checkboxGroupInput('capas', label = "", choices = c('Isóbatas'='isobatas', 'Estaciones de mejillón'='est_mej','Pecios'='pecios','Densidad AIS (2012-2013)'='dens_ais','Navegación sonar'='nav_son')
+                                                           )
+                                        )
+                          ),
+                      
+                      tags$div(id="cite",'Propuesta de estudio de la Bahía de Maldonado. Convenio PAS-DINARA.')
+                      ),
+             
+             tabPanel("Cargar SBL", icon=icon(name="file", "fa-2x"),
                       sidebarPanel(
                         strong('Archivo de Sonar de Barrido Lateral'),
                         p('Tritech StarFish 990F'),
@@ -38,7 +81,7 @@ shinyUI(navbarPage(title="Convenio PAS-DINARA",
                                                           tags$hr(),
                                                           helpText("Nota: se sugiere utilizar un nombre que contenga",
                                                                    "un identificador de la campaña y la fecha de la misma.")
-                                                          ),
+                                         ),
                                          
                                          conditionalPanel(condition="input.exportar == 'shapefile'",
                                                           strong('Nombre de Shapefile'),
@@ -47,7 +90,7 @@ shinyUI(navbarPage(title="Convenio PAS-DINARA",
                                                           tags$hr(),
                                                           helpText("Nota: se sugiere utilizar un nombre que contenga",
                                                                    "un identificador de la campaña y la fecha de la misma.")
-                                                          ),
+                                         ),
                                          
                                          conditionalPanel(condition="input.exportar == 'kmz'",
                                                           strong('Nombre de KMZ'),
@@ -66,75 +109,45 @@ shinyUI(navbarPage(title="Convenio PAS-DINARA",
                                                           tags$hr(),
                                                           helpText("Nota: se sugiere utilizar nombres que contengan",
                                                                    "un identificador de la campaña y la fecha de la misma.")
-                                                          )
                                          )
-                        ),
+                        )
+                      ),
                       
                       mainPanel(
-                          dataTableOutput("table")
-                          )
-                        ),
-                      
-                      tabPanel("SIG",
-                               icon=icon(name="map-marker", "fa-2x"),
-                                         div(class="outer",
-                                             
-                                             tags$head(
-                                               
-                                               # Include our custom CSS
-                                               includeCSS("styles.css"),
-                                               includeScript("gomap.js"),
-                                               includeScript("binding.js")
-                                               ),
-                                             
-                                             leafletMap("map", width="100%", height="100%",
-                                                        initialTileLayer = "//{s}.tiles.mapbox.com/v3/guzman.j035h3hc/{z}/{x}/{y}.png",
-                                                        initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
-                                                        
-                                                        options=list(center= c(-34.9400, -54.9700),
-                                                                     zoom = 13,
-                                                                     maxBounds = list(list(-32.99966,-49.49774), list(-40.00023,-59.50186)))
-                                                        ),
-                                             
-                                             absolutePanel(id = "controls", class="modal", fixed=TRUE, draggable=TRUE, top=120, left=300, right="auto", bottom="auto", width=200, height="auto", h4("Capas espaciales"),
-                                                           tags$hr(),
-                                                           checkboxGroupInput('capas', label = "", choices = c('Isóbatas'='isobatas', 'Estaciones de mejillón'='est_mej','Pecios'='pecios','Densidad AIS (2012-2013)'='dens_ais','Navegación sonar'='nav_son'))
-                                                           )
-                                             ),
-                               tags$div(id="cite",'Propuesta de estudio de la Bahía de Maldonado. Convenio PAS-DINARA.')
-                               ),
-             
-             tabPanel("Bitácora",                    
-                      icon=icon(name="location-arrow", "fa-2x"),
-                      
-                      fluidRow(
-                        column(2,
-                               offset=0.5,
-                               includeMarkdown("/home/guzman/GitHub/PAS-DINARA/Texto/indice.md")
-                        ),
-                        column(10,
-                               offset=-0.5,
-                               includeMarkdown("/home/guzman/GitHub/PAS-DINARA/Texto/contenido.md")
+                        dataTableOutput("table")
                         )
-                      )),
-                         
-             
-#              includeText("pas-cipac.txt")
-#              includeText("dinara.txt")
+                      ),
              
              tabPanel("Acerca de esta APP", icon=icon(name="comments", "fa-2x"),
                       sidebarPanel(
-                        HTML('<div style="clear: left;"><img src="https://raw.githubusercontent.com/guzmanlopez/PAS-DINARA/master/Iconos%20y%20figuras/Logos-PAS-DINARA.png"/></div>'), ### LOGO
-                        strong('')),
+                        HTML('<div style="clear: left;"><img src="https://raw.githubusercontent.com/guzmanlopez/PAS-DINARA/master/Iconos%20y%20figuras/Logos-PAS-DINARA.png"/></div>')
+                        ),
                       mainPanel(
-                      h3(p(strong('Descripción'))),
-                      p(style="text-align:justify",'Esta aplicación web de R con Shiny le permite al usuario visualizar y descargar la información almacenada en los archivos exportados en bruto del sonar de barrido lateral StarFish 990F de Tritech. Está aplicación está siendo desarrollada en el marco del convenio entre la Dirección Nacional de Recursos Acuáticos (DINARA) y el Programa de Arqueología Subacuática (PAS)','El objetivo es generar una herramienta que permita a los usuarios acceder fácil y rapidamente a los datos del sonar de barrido lateral luego de cada campaña.')
-                      )
+                          tabPanel("Acerca de esta APP",
+                                   h3(p(strong('Descripción'))),
+                                   p(style="text-align:justify",'Esta aplicación web de R con Shiny se encuentra en desarrollo.'),
+                                   p(style="text-align:justify",'Está siendo desarrollada en el marco del convenio entre la Dirección Nacional de Recursos Acuáticos (DINARA) y el Programa de Arqueología Subacuática (PAS). Esta aplicación web le permite al usuario seguir las actividades realizadas entre el PAS y la DINARA. Además brinda la posibilidad de visualizar y descargar la información almacenada en los archivos exportados en bruto del sonar de barrido lateral StarFish 990F de Tritech. También brinda la posibilidad de visualizar la información en un mapa interactivo. El objetivo de esta aplicación es generar una herramienta que permita reunir toda la información generada a través del convenio y facilitar su acceso mediante una interfaz amigable e interactiva; además de proporcionar herramientas que resulten de utilidad a los técnicos a la hora de procesar datos de sonar luego de cada campaña.'),
+                                   tags$hr(),
+                                   h3(p(strong('Características'))),
+                                   p(style="text-align:justify",'La mayor parte del software empleado para desarrollar esta aplicación es libre, eso quiere decir que garantiza al usuario la libertad de poder usarlo, estudiarlo, compartirlo (copiarlo) y modificarlo. El software R es un proyecto de software libre que es colaborativo y tiene muchos contribuyentes.'),
+                                   tags$hr(),
+                                   h3(p(strong('Guía de usuario'))),
+                                   HTML('<div style="clear: left;"><img src="https://raw.githubusercontent.com/guzmanlopez/Pampero/master/Figuras/PDF.png" alt="" style="width: 5%; height: 5%; float: left; margin-right:5px" /></div>'),
+                                   br(),
+                                   a('PAS-DINARA app', href="", target="_blank"),
+                                   tags$hr(),
+                                   h3(p(strong('Código fuente'))),
+                                   HTML('<div style="clear: left;"><img src="https://raw.githubusercontent.com/guzmanlopez/Pampero/master/Figuras/github-10-512.png" alt="" style="width: 5%; height: 5%; float: left; margin-right:5px" /></div>'),
+                                   br(),
+                                   a('Repositorio GitHub', href="https://github.com/guzmanlopez/PAS-DINARA", target="_blank"),
+                                   tags$hr(),
+                                   h3(p(strong('Contacto'))),
+                                   HTML('<div style="clear: left;"><img src="https://raw.githubusercontent.com/guzmanlopez/Pampero/master/Figuras/foto_perfil.jpg" alt="" style="float: left; margin-right:5px" /></div>'),
+                                   strong('Autor'),
+                                   p(a('Guzmán López', href="https://www.linkedin.com/pub/guzm%C3%A1n-l%C3%B3pez/59/230/812", target="_blank"),' - glopez@dinara.gub.uy',br(),'Biólogo | Asesor en Hidroacústica',br(),a('Laboratorio de Tecnología Pesquera - Hidroacústica (DINARA - MGAP)',href="http://www.dinara.gub.uy", target="_blank")),
+                                   br()
+                                   )
+                          )
                       )
              )
-        )
-
-
-
-
-                                 
+        )                                
